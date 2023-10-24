a6ef9574a58d25e9515f671c8cd3a8b49cec1523
Lihangfei <Lihangfei@qq.com>
2023-10-24 16:05:56 +0800

    first commit

--- src/main/java/com/li/git/git.java
+++ src/main/java/com/li/git/git.java
@@ -3,6 +3,8 @@ package com.li.git;
 public class git {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+
     }
 
 }
