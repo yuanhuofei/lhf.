45f1311381bf04f376090036574a352a9666e5b8
Lihangfei <Lihangfei@qq.com>
2023-10-31 14:45:31 +0800

    hot fix commit3

--- src/main/java/com/li/git/git.java
+++ src/main/java/com/li/git/git.java
@@ -8,6 +8,7 @@ public class git {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("master test");
 
     }
 
