e86970f3f928e62f2e53a069d786458d7f814a04
yuanhuofei <146062150+yuanhuofei@users.noreply.github.com>
2023-11-07 14:23:04 +0800

    Update git.java

--- src/main/java/com/li/git/git.java
+++ src/main/java/com/li/git/git.java
@@ -12,6 +12,7 @@ public class git {
         System.out.println("master test");
         System.out.println("hot fix test");
         System.out.println("push test");
+        System.out.println("push test1");
     }
 
 }
